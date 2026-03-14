2a281f72f0c4450f03dacfffe139849ffa01590f
Leonardo Kremer <devleonardok@gmail.com>
2026-03-14 20:15:40 -0300

    feat: add simple error handling

--- python_general/error_exception.py
+++ python_general/error_exception.py
@@ -17,3 +17,17 @@
     #action
 #except
     #exception
+    
+    
+#1 - Denominator and Numerator
+
+try: #try to execute
+    numerator = int(input("Numerator: "))
+    denominator = int(input("Denominator: "))
+    result = numerator/denominator
+except: #exception
+    print("An error occurred")
+else: #success
+    print(f"Result: {result}")
+finally: #success/exception - always happens
+    print("come back soon")
