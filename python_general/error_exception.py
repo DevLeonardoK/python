cda23330ab9b8db1051e3c332c697267ee0074b9
Leonardo Kremer <devleonardok@gmail.com>
2026-03-14 20:05:51 -0300

    docs: add initial summary and exception notes

---
+++ python_general/error_exception.py
@@ -0,0 +1,19 @@
+#Exception list
+#NameError
+#ValueError
+#ZeroDivisionError
+#TypeError
+#IndexError
+#KeyError
+#EOFError
+#KeyboardInterrupt
+#OSError
+#MemoryError
+#ConnectionError
+#RuntimeError
+
+#default code
+#try
+    #action
+#except
+    #exception
