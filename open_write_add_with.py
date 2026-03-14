0bfe0a7ce3eef1492471a8dc7852b739909d9ac7
Leonardo Kremer <devleonardok@gmail.com>
2026-03-14 12:34:39 -0300

    feat: add manual method to manipulate files and monitor RAM usage using memory_profiler

--- open_write_add_with.py
+++ open_write_add_with.py
@@ -1,2 +1,18 @@
 # This Python script demonstrates the correct way to manipulate files using 'with'.
 # It also shows the difference in memory usage and processing compared to manually opening and closing files.
+
+#--IMPORTS--
+from memory_profiler import profile
+
+
+#--LOGIC--
+@profile
+def open_file():
+    file_path = "files/text_open_write_add_with.txt"
+    file = open(file_path, 'r')
+    content = file.read()
+    file.close()
+    return content
+
+if __name__ == "__main__":
+    open_file()
