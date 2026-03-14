cf9af830def47cc2ba4a5e8a555e9b90cffaa901
Leonardo Kremer <devleonardok@gmail.com>
2026-03-14 12:15:20 -0300

    docs: add summary describing the code idea

---
+++ open_write_add_with.py
@@ -0,0 +1,2 @@
+# This Python script demonstrates the correct way to manipulate files using 'with'.
+# It also shows the difference in memory usage and processing compared to manually opening and closing files.
