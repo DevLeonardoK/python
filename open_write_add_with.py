1a9a49252f73b9c16dafb0cbad783c94759bff69
Leonardo Kremer <devleonardok@gmail.com>
2026-03-14 14:10:31 -0300

    feat: add two file reading methods using manual open and with open

--- open_write_add_with.py
+++ open_write_add_with.py
@@ -2,17 +2,32 @@
 # It also shows the difference in memory usage and processing compared to manually opening and closing files.
 
 #--IMPORTS--
-from memory_profiler import profile
+from memory_profiler import profile, memory_usage
 
 
 #--LOGIC--
+file_path = "files/text_open_write_add_with.txt"
+
+#open,read - manual
 @profile
-def open_file():
-    file_path = "files/text_open_write_add_with.txt"
+def open_file_manual():
     file = open(file_path, 'r')
     content = file.read()
     file.close()
     return content
 
+#open, read - with
+@profile
+def open_file_with():
+    with open(file_path, 'r') as file:
+        content = file.read()
+        return content
+
+
+#Execution
 if __name__ == "__main__":
-    open_file()
+    print("------------MANUAL------------")
+    open_file_manual()
+    print("\n------------WITH------------")
+    open_file_with()
+    
