f5d8c6aa26539a0d3bb43070e50de340413c5343
Leonardo Kremer <devleonardok@gmail.com>
2026-03-14 22:38:36 -0300

    feat: add get_int, get_real and input_user functions with exception handling

--- python_general/error_exception.py
+++ python_general/error_exception.py
@@ -21,6 +21,7 @@
     
 #1 - Denominator and Numerator
 
+"""
 try: #try to execute
     numerator = int(input("Numerator: "))
     denominator = int(input("Denominator: "))
@@ -37,3 +38,43 @@ else: #success
     print(f"Result: {result:.1f}")
 finally: #success/exception - always happens
     print("come back soon")
+    
+"""
+
+#2 float and int
+
+def get_int():
+    while True:
+        try:
+            int_number = int(input("Int: "))
+        except (ValueError, TypeError):
+            print("Invalid input type")
+        else:
+            return int_number
+    
+def get_real():
+    while True:
+        try:
+            real_number = float(input("Real: "))
+        except (ValueError, TypeError):
+            print("Invalid input type")
+        else:
+            return real_number
+        
+
+def input_user():
+    int = None
+    real = None
+    try:
+        int = get_int()
+        real = get_real() 
+    except KeyboardInterrupt as e:
+        print("Operation interrupted by user")
+    except Exception as exception:
+        print("An error occurred")
+    else:
+        print("Real and int number are ok")
+    finally:
+        print(f"Real number: {real} <---> Int number: {int}")
+
+input_user()
