e35691ba281ce3a0fc4c69b161b91dedcb75d0a5
Leonardo Kremer <devleonardok@gmail.com>
2026-03-15 22:30:03 -0300

    feat: add simple validation using Pydantic

--- python_general/pydantic_code.py
+++ python_general/pydantic_code.py
@@ -5,11 +5,11 @@
 
 #written in Rust
 
+from pydantic import BaseModel
 import pydantic
 print(pydantic.__version__) #2.12.5
 
 #validation without pydantic
-
 class User:
     def __init__(self, age:int, name:str):
         
@@ -19,10 +19,24 @@ class User:
             raise TypeError(f"Expected str, got {type(name).__name__}")
         
         self.age = age
-        self.name = name
-        
+        self.name = name        
 try:
-    user = User(age=19,name=12)
+    user = User(age=19,name="Leonardo")
     print(f"Name: {user.name} -- Age: {user.age}")
+except Exception as exception:
+    print(exception)
+
+
+print("\n")
+    
+    
+#validation with pydantic
+class Car(BaseModel):
+    model:str
+    year:int
+    
+try:
+    car = Car(model="Fiat Uno", year='1996')
+    print(f"Model type: {type(car.model).__name__} \n Year type: {type(car.year).__name__}")
 except Exception as exception:
     print(exception)
