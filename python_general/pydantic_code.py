7f1058b0fbae8d599d03f2410c628f2523694c4a
Leonardo Kremer <devleonardok@gmail.com>
2026-03-15 21:57:32 -0300

    chore: chore: some initial summary about pydantic

---
+++ python_general/pydantic_code.py
@@ -0,0 +1,4 @@
+#most important library for data validation & parsing
+#allows 'strict typing' by using type annotations (str,int,list...)
+
+#allows developer to define clear and secure data models
